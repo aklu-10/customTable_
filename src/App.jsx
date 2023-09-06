9eeb4d017e2f93f5e8b1ad3009d2b7c17cf1f31c
Akash Kumar <akash@unicodesystems.info>
2023-09-06 18:44:33 +0530

    custom table update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ const App = () => {
 
       <DynamicTable/>
 
+      <div style={{margin:'20px', border:'1px solid black', padding:'20px'}}>
+        <h3>Practiced Adding Custom Column with menu in DataGrid (showcase image below) </h3>
+
+        <img src='./practiced_custom_column_with_menu.png' style={{border:'1px solid red'}}/>
+      </div>
+
     </>
 
   )
